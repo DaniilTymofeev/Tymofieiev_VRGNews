aa9b8b2b3f35b6c6dbba48e430d2551764665cd9
Danylo Tymofieiev <tymofeevdaniil.work@gmail.com>
2025-09-17 22:27:01 +0300

    Project init
    Project init

---
+++ VRGNews/VRGNews/VRGNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VRGNewsApp.swift
+//  VRGNews
+//
+//  Created by Danil Tymofeev on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct VRGNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
